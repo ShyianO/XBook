55df41fce0ae27a4b210a38d17f5db85145916ed
ShyianO <ol.shyian@gmail.com>
2021-03-15 10:30:51 +0200

    Backendless was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,26 @@ import { TodoState } from './store/todo.state';
 import { LandingModule } from './landing/landing.module';
 import { SharedModule } from './shared/shared.module';
 
+import Backendless from 'backendless';
+
+const APP_ID = '2FA3E688-ADE5-556D-FFA2-2F42B5F5FB00';
+const API_KEY = 'CB2A4850-B144-4F23-9C45-5C4D2BC2FF04';
+
+Backendless.initApp(APP_ID, API_KEY);
+
+class AppUser extends Backendless.User {
+  name: string;
+}
+
+const user: AppUser = new AppUser();
+user.email = 'michael@backendless.com';
+user.password = 'my_super_password';
+user.name = 'Michael';
+
+Backendless.UserService.register<AppUser>(user)
+  .then((result: AppUser) => console.log('Registered User:', result))
+  .catch((error) => console.error('Can not Register User:', error.message));
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
