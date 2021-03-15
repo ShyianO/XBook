1ff154d4ebd52d6efee5b05672556a56df9489ee
ShyianO <ol.shyian@gmail.com>
2021-03-15 16:47:51 +0200

    Moved APP ID and KEY to environmets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,21 @@ import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import Backendless from 'backendless';
+
+import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { TodoState } from './store/todo.state';
+import { AppUserService } from './app-user.service';
 
 import { LandingModule } from './landing/landing.module';
 import { SharedModule } from './shared/shared.module';
 
-import Backendless from 'backendless';
-
-const APP_ID = '2FA3E688-ADE5-556D-FFA2-2F42B5F5FB00';
-const API_KEY = 'CB2A4850-B144-4F23-9C45-5C4D2BC2FF04';
-
-Backendless.initApp(APP_ID, API_KEY);
-
-class AppUser extends Backendless.User {
-  name: string;
-}
-
-const user: AppUser = new AppUser();
-user.email = 'michael@backendless.com';
-user.password = 'my_super_password';
-user.name = 'Michael';
-
-Backendless.UserService.register<AppUser>(user)
-  .then((result: AppUser) => console.log('Registered User:', result))
-  .catch((error) => console.error('Can not Register User:', error.message));
+Backendless.initApp(
+  environment.backendless.APP_ID,
+  environment.backendless.API_KEY
+);
 
 @NgModule({
   declarations: [AppComponent],
@@ -46,7 +34,7 @@ Backendless.UserService.register<AppUser>(user)
     NgxsReduxDevtoolsPluginModule.forRoot(),
     NgxsLoggerPluginModule.forRoot()
   ],
-  providers: [],
+  providers: [AppUserService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
