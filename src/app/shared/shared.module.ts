d1cb4c2c8a87d85f0384d1f87f049c84a6d25c2e
ShyianO <ol.shyian@gmail.com>
2021-03-16 13:48:18 +0200

    Configured header and footer components, added logo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -18,8 +17,7 @@ const materialModules = [
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, ...materialModules],
-  exports: [...materialModules]
+  imports: [materialModules],
+  exports: [materialModules]
 })
 export class SharedModule {}
