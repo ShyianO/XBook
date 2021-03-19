66c48dfda59602cd6cea75464b5c14e60afe024d
ShyianO <ol.shyian@gmail.com>
2021-03-19 15:58:36 +0200

    Added formGroup and configured register components

--- src/app/landing/components/register/register.component.ts
+++ src/app/landing/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { IRegister } from './register.interface';
 
 @Component({
   selector: 'app-register',
@@ -9,54 +11,23 @@ import { FormControl, Validators } from '@angular/forms';
 export class RegisterComponent {
   hide = true;
 
-  name = new FormControl('', [
-    Validators.required,
-    Validators.maxLength(30),
-    Validators.pattern('[a-zA-Z ]*')
-  ]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [
-    Validators.required,
-    Validators.maxLength(30),
-    Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,20}')
-  ]);
+  loginForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(30),
+      Validators.pattern('[a-zA-Z ]*')
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(30),
+      Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,20}')
+    ])
+  });
 
-  getNameError(): string {
-    if (this.name.hasError('required')) {
-      return 'You must enter a value';
+  onSubmit({ value, valid }: { value: IRegister; valid: boolean }): void {
+    if (valid) {
+      alert(`Welcome, ${value.name}`);
     }
-
-    if (this.name.hasError('pattern') || this.name.hasError('maxlength')) {
-      return 'Not a valid name';
-    }
-
-    return '';
-  }
-
-  getEmailError(): string {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  getPasswordError(): string {
-    if (this.password.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    if (
-      this.password.hasError('pattern') ||
-      this.password.hasError('maxlength')
-    ) {
-      return 'Not a valid password';
-    }
-
-    return '';
-  }
-
-  submitHandler(): void {
-    alert(`Welcome!`);
   }
 }
