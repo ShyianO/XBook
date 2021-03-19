cbf4aba4ead1ee34c9504329fedd5fe913a2b36c
ShyianO <ol.shyian@gmail.com>
2021-03-19 18:17:36 +0200

    Changed react form controls

--- src/app/landing/components/register/register.component.ts
+++ src/app/landing/components/register/register.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 import { IRegister } from './register.interface';
 
@@ -8,22 +13,37 @@ import { IRegister } from './register.interface';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   hide = true;
+  loginForm: FormGroup;
 
-  loginForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.maxLength(30),
-      Validators.pattern('[a-zA-Z ]*')
-    ]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.maxLength(30),
-      Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,20}')
-    ])
-  });
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30),
+        Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,20}')
+      ])
+    });
+  }
+
+  get name(): AbstractControl {
+    return this.loginForm.get('name');
+  }
+
+  get email(): AbstractControl {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl {
+    return this.loginForm.get('password');
+  }
 
   onSubmit({ value, valid }: { value: IRegister; valid: boolean }): void {
     if (valid) {
