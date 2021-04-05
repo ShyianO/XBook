0b8d8cb80ed9d3eb25b0ec51bb590ba3b0b8fc5a
ShyianO <ol.shyian@gmail.com>
2021-04-05 18:16:20 +0300

    Testing deploy action

--- deploy.js
+++ deploy.js
@@ -7,13 +7,13 @@ const fs = require('fs');
     console.log('Building...');
 
     await execa('npm', ['run', 'build']);
-    const folderName = fs.existsSync('dist') ? 'dist' : 'build';
-    await execa('git', ['--work-tree', folderName, 'add', '--all']);
-    await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
+
+    await execa('git', ['--work-tree', 'dist', 'add', '--all']);
+    await execa('git', ['--work-tree', 'dist', 'commit', '-m', 'gh-pages']);
     console.log('Pushing to gh-pages...');
 
     await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
-    await execa('rm', ['-r', folderName]);
+    await execa('rm', ['-r', 'dist']);
     await execa('git', ['checkout', '-f', 'master']);
     await execa('git', ['branch', '-D', 'gh-pages']);
     console.log('Successfully deployed');
